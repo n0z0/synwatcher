433f8bac1fde5ae3b2d7a6654971db8a69224741
Rolly Maulana Awangga <awangga@gmail.com>
2025-11-04 02:45:20 +0700

    ganti port ke 2025

--- scp/main.go
+++ scp/main.go
@@ -20,7 +20,7 @@ const (
 	cacheDB        = "127.0.0.1:50051" // gRPC cache DB address
 	privateKeyPath = "id_rsa"          // host key file
 	host           = "0.0.0.0"
-	port           = "2022"
+	port           = "2025"
 )
 
 func main() {
